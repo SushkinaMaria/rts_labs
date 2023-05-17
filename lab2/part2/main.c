e3a90c804bf1d166561abdc04e4189d73524c731
SushkinaMaria <sushkina.mariah@yandex.ru>
2023-05-17 23:47:39 +0300

    Fix lab2

--- lab2/part2/main.c
+++ lab2/part2/main.c
@@ -9,15 +9,17 @@
 pthread_t thread_id1;
 pthread_t thread_id2;
 
-pthread_attr_t thread_attr1;
-pthread_attr_t thread_attr2;
+struct sched_param param1 = {10};
+struct sched_param param2 = {8};
 
 #define PTHREADS_NUM 2
 
 int wait_threads(pthread_t *pthreads, int pthreads_num)
 {
-    for (int i=0; i<pthreads_num; i++) {
-        int ret = pthread_join(pthreads[i], NULL);
+    int ret;
+    int i;
+    for (i=0; i<pthreads_num; i++) {
+        ret = pthread_join(pthreads[i], NULL);
         if (ret) {
             return ret;
         }
@@ -28,36 +30,24 @@ int wait_threads(pthread_t *pthreads, int pthreads_num)
 int main(int argc, char const *argv[])
 {
     int ret;
+    pthread_t *pthreads_to_wait;
     printf("Prog threads PID %d\n", getpid());
 
-    pthread_attr_init(&thread_attr1);
-    pthread_attr_init(&thread_attr2);
-
-    ret = pthread_attr_setschedpolicy(&thread_attr1, SCHED_RR);
-    if (ret) {
-        perror("setschedpolicy thread 1 error");
-        return -1;
-    }
-    ret = pthread_attr_setschedpolicy(&thread_attr2, SCHED_FIFO);
-    if (ret) {
-        perror("setschedpolicy thread 2 error");
-        return -1;
-    }
-    ret = pthread_setschedprio(thread_id1, 10);
+    ret = pthread_setschedparam(thread_id1, SCHED_RR, &param1);
     if (!ret) {
         printf("setschedprio thread 1 error: %d\n", ret);
         return -1;
     }
-    ret = pthread_setschedprio(thread_id2, 8);
+    ret = pthread_setschedparam(thread_id2, SCHED_FIFO, &param2);
     if (!ret) {
         printf("setschedprio thread 2 error: %d\n", ret);
         return -1;
     }
 
-    pthread_create(&thread_id1, &thread_attr1, long_thread1, NULL);
-    pthread_create(&thread_id2, &thread_attr2, long_thread2, NULL);
+    pthread_create(&thread_id1, NULL, long_thread1, NULL);
+    pthread_create(&thread_id2, NULL, long_thread2, NULL);
 
-    pthread_t *pthreads_to_wait = (pthread_t*) malloc(PTHREADS_NUM*sizeof(pthread_t));
+    pthreads_to_wait = (pthread_t*) malloc(PTHREADS_NUM*sizeof(pthread_t));
     pthreads_to_wait[0] = thread_id1;
     pthreads_to_wait[1] = thread_id2;
 
